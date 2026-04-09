b5ddc6726954425f8805f6c2fcc2ae1b9551d3f5
dabbingt1 <yehijhan0614@gmail.com>
2026-04-10 00:51:25 +0800

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -48,17 +48,43 @@ public class Customer {
 
     //get set
 
-    public String getFirstName(){
-        return this.firstName;
-    }
+     public String getFirstName() {
+    return this.firstName;
+  }
 
-    public static void main(String[] args) {
-        // "new" -> produce object ( call constructor)
-        Customer c1 = new Customer();
+  public static void main(String[] args) {
+    // "new" -> produce object (call constructor)
+    //
+    Customer c1 = new Customer();
+    Customer c2 = new Customer("John", "Chan", 'M', LocalDate.of(1990, 1, 1));
+    System.out.println(c2.getFirstName()); // John
 
-        Customer c2 = new Customer("John", "Chan", 'M', LocalDate.of(1990, 1, 1));
-        System.out.println(c2.getFirstName());
-        System.out.println(c2.getBirthYear());
-        //System.out.println(c2.getDob().getYear());
+    System.out.println(c1.getFirstName()); // null
+
+    System.out.println(c2.getBirthYear()); // 1990
+    //System.out.println(c2.getDob().getYear()); // 1990
+
+
+    Customer c4 = new Customer();
+    Order o1 = new Order();
+    
+    // 2 orders
+    // order 1: 2 items
+    // order 2: 1 item
+
+
+    Item i1 = new Item("iphone19", 21000, 3);
+    Item i2 = new Item("ak14", 120000, 1);
+    Order or1 = new Order(new Item[]{i1, i2});
+
+    Item i3 = new Item("dog", 120, 15);
+    Order or2 = new Order(new Item[]{i3});
+
+    c4.orders = new Order[]{or1, or2};
+
+    System.out.println("Order amount : " + c4.totalOrderAmount());
+    System.out.println("Is VIP: " + c4.isVip());
+
+    
     }
 }
